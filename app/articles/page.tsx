2d5b1c12011a1029933fbe43c9cbe2090d20da58
codenamezeta <michael@a2zeta.com>
2025-01-21 16:49:33 -0800

    Updates Articles page with placeholder content

---
+++ app/articles/page.tsx
@@ -0,0 +1,9 @@
+// Ensure you have a proper export statement
+export default function ArticlesPage() {
+  return (
+    <div>
+      <h1>Articles</h1>
+      {/* Your content here */}
+    </div>
+  )
+}
