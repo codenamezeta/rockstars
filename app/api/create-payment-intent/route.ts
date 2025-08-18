a012e943cd6381bb59fb93a97ca6960004d2b575
codenamezeta <michael@a2zeta.com>
2025-08-17 20:28:32 -0700

    Safely disables Stripe API routes while not in use.

--- app/api/create-payment-intent/route.ts
+++ app/api/create-payment-intent/route.ts
@@ -1,23 +1,23 @@
-import { NextResponse } from 'next/server'
-import Stripe from 'stripe'
+// import { NextResponse } from 'next/server'
+// import Stripe from 'stripe'
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  apiVersion: '2025-07-30.basil',
-})
+// const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+//   apiVersion: '2025-07-30.basil',
+// })
 
-export async function POST(req: Request) {
-  try {
-    const { amount } = await req.json()
+// export async function POST(req: Request) {
+//   try {
+//     const { amount } = await req.json()
 
-    // Create a PaymentIntent with the order amount and currency
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount: amount * 100, // Stripe requires the amount in cents
-      currency: 'usd',
-      automatic_payment_methods: { enabled: true },
-    })
+//     // Create a PaymentIntent with the order amount and currency
+//     const paymentIntent = await stripe.paymentIntents.create({
+//       amount: amount * 100, // Stripe requires the amount in cents
+//       currency: 'usd',
+//       automatic_payment_methods: { enabled: true },
+//     })
 
-    return NextResponse.json({ clientSecret: paymentIntent.client_secret })
-  } catch (error: any) {
-    return NextResponse.json({ error: error.message }, { status: 400 })
-  }
-}
+//     return NextResponse.json({ clientSecret: paymentIntent.client_secret })
+//   } catch (error: any) {
+//     return NextResponse.json({ error: error.message }, { status: 400 })
+//   }
+// }
